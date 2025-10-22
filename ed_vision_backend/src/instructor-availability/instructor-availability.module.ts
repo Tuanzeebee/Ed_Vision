cfe8432f41494e91a036747502eb012fb0675962
Tuanzeebee <tuanzeebee@gmail.com>
2025-10-23 01:52:04 +0700

    fix:miss import file

--- ed_vision_backend/src/instructor-availability/instructor-availability.module.ts
+++ ed_vision_backend/src/instructor-availability/instructor-availability.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { InstructorAvailabilityController } from './instructor-availability.controller';
 import { InstructorAvailabilityService } from './instructor-availability.service';
 import { InstructorAvailabilityRepository } from './instructor-availability.repository';
-import { DebugController } from './test-debug.controller';
 
 @Module({
-  controllers: [InstructorAvailabilityController, DebugController],
+  controllers: [InstructorAvailabilityController],
   providers: [
     InstructorAvailabilityService,
     InstructorAvailabilityRepository,
