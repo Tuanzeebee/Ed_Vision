44ae445735c8548b8c3f7a33905b90ab87cad8f5
Tuanzeebee <tuanzeebee@gmail.com>
2025-11-20 19:25:53 +0700

    feat():connect mongodb database

--- ed_vision_backend/src/app.module.ts
+++ ed_vision_backend/src/app.module.ts
@@ -13,7 +13,8 @@ import { StatisticsOverviewModule } from './admin_be/statistics-overview/statist
 import { BookingModule } from './booking/booking.module';
 import { ClassManagementModule } from './teacher_be/class-management/class-management.module';
 import { SurveysModule } from './teacher_be/surveys/surveys.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './mongodb/database.module';
 @Module({
   imports: [
     PrismaModule,
@@ -28,6 +29,8 @@ import { SurveysModule } from './teacher_be/surveys/surveys.module';
     StatisticsOverviewModule,
     ClassManagementModule,
     SurveysModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
